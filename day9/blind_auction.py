b98bfbf5b59b2add04df026accad550ffe401a4b
Dhanush340 <dhanushkathilan1996@gmail.com>
2022-04-30 21:43:29 +0530

    secret bid answer added

---
+++ day9/blind_auction.py
@@ -0,0 +1,26 @@
+print("Welcome to the secret auction program")
+bidding = False
+bids = {}
+
+
+def find_highest_bid(bids):
+    high_bid = 0
+    winner = ""
+    for bidder in bids:
+        if bids[bidder] > high_bid:
+            high_bid = bids[bidder]
+            winner = bidder
+    print(f"winner is {winner} and bid is ${high_bid}")
+
+
+while not bidding:
+    name = input("what is your Name: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    any_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
+
+    if any_bidders == 'no':
+        bidding = True
+        find_highest_bid(bids)
+    elif any_bidders == "yes":
+        bidding = False
